58bb60b657ba062bd07019384e1c87f0ab244736
PranavSikarwal <sikarwalpranav123@gmail.com>
2023-09-16 10:55:44 +0530

    made changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
-import styles from '../styles/Home.module.css'
-import ScanComponent  from "../components/UIcomponents/ScanComponent"; 
+import styles from "../styles/Home.module.css";
+import ScanComponent from "../components/UIcomponents/ScanComponent";
 
 const Home = () => {
-  return <>
-    <div classname={styles.animationContainer}>
-      Animation here...
+  return (
+    <div className={styles.homePage}>
+      <div className={styles.video}>
+        video here...
+      </div>
+      <div className={styles.scanContainer}>
+        <ScanComponent classname={styles.scanComponent} />
+      </div>
     </div>
-    <div>
-      <ScanComponent />
-    </div>
-  </>;
+  );
 };
 
 export default Home;
